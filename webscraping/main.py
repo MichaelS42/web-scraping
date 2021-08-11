d9ff5ab5fb7a7f5872700cfc51804cbd77607389
Michael Sanchez <michaelsanchez42@live.com>
2021-08-10 19:43:14 -0600

    adding published date

--- webscraping/main.py
+++ webscraping/main.py
@@ -7,4 +7,6 @@ html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?se
 soup = BeautifulSoup(html_text, 'lxml')
 job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
 company_name = job.find('h3', class_ = 'joblist-comp-name')
-skills = job.find('span', class_ = 'srp-skills').text
+skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
+published_date = job.find('span', class_ = 'sim-posted').span.text
+print(published_date)
