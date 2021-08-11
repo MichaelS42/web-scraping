dfc0039eeb7e44fe83fbf0c94ea3c43f234e926b
Michael Sanchez <michaelsanchez42@live.com>
2021-08-10 19:38:41 -0600

    adding

--- webscraping/main.py
+++ webscraping/main.py
@@ -2,8 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 
 
-html_text = requests.get('')
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=')
 
 soup = BeautifulSoup(html_text, 'lxml')
-data = soup.find('li', class_ = 'js inspect elements')
-data_name = job.find('element', class_ = 'classname')
+job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
+company_name = job.find('h3', class_ = 'joblist-comp-name')
+skills = job.find('span', class_ = 'srp-skills').text
